bd7b8281aa948e0c26a22fd3076832ea61143935
fernandomg <fernando.greco@gmail.com>
2021-03-17 15:28:28 -0300

    Adapt spending Limit modal to Generic Modal

--- src/routes/safe/components/Settings/SpendingLimit/NewLimitModal/Review.tsx
+++ src/routes/safe/components/Settings/SpendingLimit/NewLimitModal/Review.tsx
@@ -2,9 +2,9 @@ import { Button, Text } from '@gnosis.pm/safe-react-components'
 import React, { ReactElement, useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import Block from 'src/components/layout/Block'
 import Col from 'src/components/layout/Col'
 import Row from 'src/components/layout/Row'
+import { Modal } from 'src/components/Modal'
 import { getNetworkInfo } from 'src/config'
 import { createTransaction, CreateTransactionArgs } from 'src/logic/safe/store/actions/createTransaction'
 import { SafeRecordProps, SpendingLimit } from 'src/logic/safe/store/models/safe'
@@ -23,7 +23,6 @@ import { fromTokenUnit, toTokenUnit } from 'src/logic/tokens/utils/humanReadable
 import { sameAddress, ZERO_ADDRESS } from 'src/logic/wallets/ethAddresses'
 import { RESET_TIME_OPTIONS } from 'src/routes/safe/components/Settings/SpendingLimit/FormFields/ResetTime'
 import { AddressInfo, ResetTimeInfo, TokenInfo } from 'src/routes/safe/components/Settings/SpendingLimit/InfoDisplay'
-import Modal from 'src/routes/safe/components/Settings/SpendingLimit/Modal'
 import { useStyles } from 'src/routes/safe/components/Settings/SpendingLimit/style'
 import { safeParamAddressFromStateSelector, safeSpendingLimitsSelector } from 'src/logic/safe/store/selectors'
 import { TxParameters } from 'src/routes/safe/container/hooks/useTransactionParameters'
@@ -245,9 +244,9 @@ export const ReviewSpendingLimits = ({ onBack, onClose, txToken, values }: Revie
     >
       {(txParameters, toggleEditMode) => (
         <>
-          <Modal.TopBar title="New Spending Limit" titleNote="2 of 2" onClose={onClose} />
+          <Modal.Header title="New Spending Limit" titleNote="2 of 2" onClose={onClose} />
 
-          <Block className={classes.container}>
+          <Modal.Body>
             <Col margin="lg">
               <AddressInfo address={values.beneficiary} title="Beneficiary" />
             </Col>
@@ -288,7 +287,7 @@ export const ReviewSpendingLimits = ({ onBack, onClose, txToken, values }: Revie
               isTransactionCreation={isCreation}
               isTransactionExecution={isExecution}
             />
-          </Block>
+          </Modal.Body>
           <div className={classes.gasCostsContainer}>
             <TransactionFees
               gasCostFormatted={gasCostFormatted}
